622b67b2f350b0588cd87aadf3692282edd6d017
Meg McCauley <memi.mc14@gmail.com>
2022-02-24 12:37:06 -0700

    added code

--- server/server.js
+++ server/server.js
@@ -17,6 +17,21 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+let students = []
+
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('student was added successfully', {author: 'Meg', type: 'manual', student: name})
+
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
+
 const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
