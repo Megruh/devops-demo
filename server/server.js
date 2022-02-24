ebf80af1b9daf4c7f9df7b1280239a7bf4e3247f
Meg McCauley <memi.mc14@gmail.com>
2022-02-24 14:14:40 -0700

    updated to match demo code

--- server/server.js
+++ server/server.js
@@ -11,8 +11,6 @@ const rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-
-
 const app = express()
 
 app.use(express.json())
@@ -29,11 +27,20 @@ app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
 
-    students.push(name)
+    const index = students.findIndex(studentName=> studentName === name)
 
-    rollbar.log('student was added successfully', {author: 'Meg', type: 'manual', student: name})
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
 
-    res.status(200).send(students)
 })
 
 app.use(rollbar.errorHandler())
